7392b49880e23b5cc51d46b34eb57b269b3310af
leejw951208 <leejw951208@gmail.com>
2023-12-26 02:20:18 +0900

    경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -13,37 +13,33 @@ import { FindPassword } from "./pages/auth/find-password"
 import Join from "./pages/auth/join"
 import Anonymous from "./components/layout/anonymous"
 
-console.log(import.meta.env)
-
 const router = createBrowserRouter([
   {
-    path: `${import.meta.env.BASE_URL}`,
     element: <Protected><Layout /></Protected>,
     children: [
       {
-        path: "",
+        path: `${import.meta.env.BASE_URL}`,
         element: <Home />,
       },
       {
-        path: "profile",
+        path: `${import.meta.env.BASE_URL}profile`,
         element: <Profile />,
       }
     ]
   },
   {
-    path: `${import.meta.env.BASE_URL}`,
     element: <Anonymous><AnonymousLayout /></Anonymous>,
     children: [
       {
-        path: "login",
+        path: `${import.meta.env.BASE_URL}login`,
         element: <Login />
       },
       {
-        path: "join",
+        path: `${import.meta.env.BASE_URL}join`,
         element: <Join />
       },
       {
-        path: "find-password",
+        path: `${import.meta.env.BASE_URL}find-password`,
         element: <FindPassword />
       }
     ]
