9d5bb0d512546675308ea45eacfe1309bacc592c
leejw951208 <leejw951208@gmail.com>
2023-12-26 01:58:27 +0900

    경로 설정

--- src/components/layout/protected.tsx
+++ src/components/layout/protected.tsx
@@ -6,8 +6,8 @@ export default function Protected({children}: {children: React.ReactNode}) {
   if (user) {
     if (user.providerData[0].providerId === "password" && !user.emailVerified) {
       auth.signOut();
-      return <Navigate to="/login" />
+      return <Navigate to={`${import.meta.env.BASE_URL}login`} />
     }
-  } else return <Navigate to="/login" />;
+  } else return <Navigate to={`${import.meta.env.BASE_URL}login`} />
   return children;
 }
